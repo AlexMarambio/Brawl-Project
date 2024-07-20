bd1b7e455a73c0bc5718de11d3e84d9e8620c3f4
Alex Marambio <65684793+AlexMarambio@users.noreply.github.com>
2024-07-20 13:36:33 -0400

    Base-Game

---
+++ BaCharacter.gd
@@ -0,0 +1,20 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		$AnimationPlayer.play("Walk")
+	else:
+
+		$AnimationPlayer.play("FightPosition")
+
